debd4a6c049b7c9a6065dfc248b7a1482136f129
simon <lashwang@outlook.com>
2018-02-27 15:36:30 +0800

    updata.

--- logcat_grep_wrapper.py
+++ logcat_grep_wrapper.py
@@ -219,7 +219,7 @@ def read_aggregated_file(aggregated_log_file,on_user_filter=None,on_logcat_filte
                 continue
 
             pckuserId = normalize_userid(pckuserId)
-            print "read user {} block".format(pckuserId)
+            #print "read user {} block".format(pckuserId)
             if not on_user_filter or on_user_filter(pckuserId,pck_start_time,pck_end_time):
                 read_block(binaryFile, pckPayloadSize,pckuserId,on_logcat_filter)
         # end while
@@ -275,6 +275,7 @@ class MainWrapper(object):
         query_user_first_upgrage_time()
         file_list = read_log_file_list_with_upc_time(global_upc_df)
         for file in file_list:
+            print file
             read_aggregated_file(file,global_on_user_filter,global_on_logcat_filter)
         pass
 
